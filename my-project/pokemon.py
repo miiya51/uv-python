904edc64641283d09740b6c485448803f7c398c1
miiya51 <fw551cp1@gmail.com>
2024-09-28 15:59:00 +0900

    feat: pokeapiリクエスト機能

---
+++ my-project/pokemon.py
@@ -0,0 +1,54 @@
+import requests
+
+url = "https://pokeapi.co/api/v2/pokemon/"
+
+print('ポケモン図鑑番号を入れてください')
+# 入力文字を取得
+poke_id = input('>> ')
+
+# バリデーションチェック
+while True:
+    # 範囲は1〜905 (2022/10現在)
+    judge = 0 <= int(poke_id) < 906
+
+    if not judge:
+        # 存在しないIDを記述したら、再入力を求める
+        print('存在しないポケモンIDを入力しています！再度入力してください。')
+        poke_id = input('>> ')
+
+    else:
+        # 正しければループを抜ける
+        break
+
+# urlに図鑑IDを付与
+url = url + poke_id
+
+response = requests.get(url)
+response = response.json()
+
+# 名前
+name = response['name']
+# ID
+id = response['id']
+# ポケモン画像
+image = response['sprites']['front_default']
+# タイプ
+types = response['types'][0]['type']['name']
+
+def get_pokemon_japanese_name(english_name):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{english_name.lower()}")
+    if response.ok:
+        data = response.json()
+        
+        for name_info in data['names']:
+            if name_info['language']['name'] == 'ja-Hrkt':
+                return name_info['name']
+        return "日本語名が見つかりません。"
+    else:
+        return "ポケモンの情報を取得できませんでした。"
+print(id)
+print(name)
+japanese_name = get_pokemon_japanese_name(name)
+print(japanese_name)
+print(image)
+print(types)
